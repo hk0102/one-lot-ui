92bb6b167e5d5088513e09fae7a25de3eedb15f7
Harpreet Kaur <151753237+hk0102@users.noreply.github.com>
2024-11-12 23:24:30 +0530

    Minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import AboutUs from "./components/AboutUs";
-import FinancingOptions from "./components/FinancingSection"
+import FinancingOptions from "./components/FinancingOptions"
 import DealershipManagement from "./components/DealershipManagement";
 import Benefits from "./components/Benefits";
-import CustomerTestimonials from "./components/TestimonialSection";
+import CustomerTestimonials from "./components/CustomerTestimonials";
 import SocialMediaConnect from "./components/SocialMediaConnect";
 import FAQsSection from "./components/FAQsSection";
-import PageFooter from "./components/FooterSection";
-
+import PageFooter from "./components/PageFooter";
 
 export default function Home() {
   return (
